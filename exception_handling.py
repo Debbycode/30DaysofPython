aacc4ddfdeb99d8607efd23cfe3334f176670aa4
Deborah Ajah <68581158+Debbycode@users.noreply.github.com>
2023-02-13 11:25:46 +0100

    day_17

---
+++ exception_handling.py
@@ -0,0 +1,11 @@
+
+print('==================One==================')
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+nordic_countries = names[:5]
+es = names[5]
+ru = names[6]
+
+print("Nordic countries:", nordic_countries)
+print("Estonia:", es)
+print("Russia:", ru)
